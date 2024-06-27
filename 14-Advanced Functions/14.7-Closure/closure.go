a4b9512fec6a675099deeb54ee5888c0d0b0a7d8
crizostomo <diogo_crizostomo@hotmail.com>
2024-06-27 15:56:04 -0300

    feat: Advanced Functions: Closure

---
+++ 14-Advanced Functions/14.7-Closure/closure.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func closure() func() {
+	text := "Inside closure"
+
+	function := func() {
+		fmt.Println(text)
+	}
+
+	return function
+}
+
+func main() {
+
+	text := "Inside main"
+	fmt.Println(text)
+
+	newFunction := closure()
+	newFunction()
+
+}
